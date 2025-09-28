1bbb37333b398ff4265cfbf62e575e06711219a5
Wouter Van Schandevijl <woutervs@hotmail.com>
2025-09-29 00:35:31 +0200

    file-management: reference libs

--- apps/file-management/src/app/page.tsx
+++ apps/file-management/src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@itenium/ui";
+
 export default function Index() {
   return (
     <div>
       File Management
+      <div>
+        <Button>test</Button>
+      </div>
     </div>
   );
 }
